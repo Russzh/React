abb9cad8ccbe40ac3ca612a4171820b4a8d6fff1
Russzh <47798938+Russzh@users.noreply.github.com>
2019-04-30 00:21:40 +0300

    Broken down into component tasks

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-    <div>
-        <h1>My Todo List</h1>
-        <input placeholder ="search" />
+const TodoList = () => {
+    const items = ['Learn React','Build Awesome App']
+    return (
         <ul>    
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
+            <li>{ items[0] }</li>
+            <li>{ items[1] }</li>
         </ul>
-    </div>
-);
+    );
+};
 
-ReactDOM.render(el,
+const AppHeader = () => {
+    return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+    const searchText = 'Type here to search'
+    return <input placeholder = { searchText}/>;
+};
+
+const App = () => {
+    return (
+        <div> 
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+    </div>
+    );
+};
+
+ReactDOM.render(<App />,
      document.getElementById("root")); 
