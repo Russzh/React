cd7aea306d7eff0d135989d18793ec526acbdcfa
Russzh <47798938+Russzh@users.noreply.github.com>
2019-05-04 11:36:52 +0300

    Arrays are props in components

--- todo/src/components/todo-list.js
+++ todo/src/components/todo-list.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import TodoListItem from './todo-list-item';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
    // const items = ['Learn React','Build Awesome App']
     return (
         <ul>    
-            <li><TodoListItem label = "Drink Coffee"/></li>
-            <li><TodoListItem 
-            label = "Build Awesome App" 
-            important = {true}/></li>
+            <li>
+                <TodoListItem label =   {todos[0].label}
+                important = {todos[0].important}/>
+            </li>
+            <li>
+                <TodoListItem label =   {todos[1].label}
+                important = {todos[1].important}/>
+            </li>
         </ul>
     );
 };
